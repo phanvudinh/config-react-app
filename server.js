6b709ba48ac425e7273c608eb3c5ddcdf49563ab
Dinh Phan Vu <itphanvudinh@gmail.com>
2018-03-01 01:12:10 +0700

    Gzip file

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var compression = require('compression');
+
+// using static Gzip
+// app.use(compression())
+
+app.get('*.js|html|css', function (req, res, next) {
+    console.log("gzip",req.url);
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    next();
+});
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res) {
-    console.log(req.url)
+    console.log("*",req.url);
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,19 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
+var CompressionPlugin = require('compression-webpack-plugin');
 
 const prod = merge(common, {
   plugins: [
-    new UglifyJSPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin()
   ]
 });
 
