4185fbee4e3dc0e04fd3bd129af5453e3309ff6a
Dinh Phan <dinh.phan@axonactive.com>
2018-03-08 13:42:06 +0700

    add path

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path');
 
 const dev = merge(common, {
   output: {publicPath: '/'},
